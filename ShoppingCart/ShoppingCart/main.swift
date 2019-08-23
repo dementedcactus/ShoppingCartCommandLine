1474e8293144cd05e580993d90def916c67cff17
Richard Crichlow <dementedcactus@gmail.com>
2019-08-23 17:40:21 -0400

    initial commit

---
+++ ShoppingCart/ShoppingCart/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ShoppingCart
+//
+//  Created by Richard Crichlow on 8/23/19.
+//  Copyright © 2019 Richard Crichlow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
